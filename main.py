f357d9e175034dde7138a5734eebd730c9ce1d33
Daksh Gupta <dakshkumargupta@gmail.com>
2017-07-09 21:32:03 -0500

    Rows can be inserted through the web page

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config
 import alchemy as al
+from datetime import date, datetime
+
+session = None
 
 @view_config(route_name='add', renderer='/templates/add.jinja2')
 def add_record(request):
@@ -9,8 +12,57 @@ def add_record(request):
 
 @view_config(route_name='add', renderer='/templates/add.jinja2', request_method='POST')
 def submitted(request):
-    print(request.params)
-    return {'success':'','failure':'hidden'}
+    pr = request.params
+    args = {
+    # 'app': date(pr['date']),
+    'comp': pr['company'].strip(),
+    'pos': pr['position'].strip(),
+    'resp': False
+    }
+
+    # print(pr['date'])
+    year, month, day = [int(dt) for dt in pr['date'].split('-')]
+    dt = date(year=year, month=month, day=day)
+    # print(dt)
+    args['app'] = dt
+
+    if pr['pid'] != '':
+        args['p_id'] = pr['pid'].strip()
+
+    if pr['location'] != '':
+        args['loc'] = pr['location'].strip()
+    
+    if pr['country'] == 'Other':
+        args['country'] = pr['other_country'].strip()
+    else:
+        args['country'] = pr['country']
+    
+    if pr['portals'] == 'Other':
+        args['portal'] = pr['other_portals'].strip()
+    else:
+        args['portal'] = pr['portals']
+
+    skills = []
+    if 'skills' in pr:
+        skills += pr.getall('skills')
+
+    if pr['other_skills'] != '':
+        temp = [skill.strip() for skill in pr['other_skills'].split(',')]
+        skills += temp
+
+    args['skills'] = skills
+
+    template_args = {}
+    try:
+        al.insert_job(session, args)
+        template_args['success'] = ''
+        template_args['failure'] = 'hidden'
+    except Exception as e:
+        print(e)
+        template_args['success'] = 'hidden'
+        template_args['failure'] = ''
+    finally:
+        return template_args
 
 @view_config(route_name='search', renderer='/templates/search.jinja2')
 def search_record(request):
